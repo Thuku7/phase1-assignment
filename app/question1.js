c93be7e335e33cc210b46ac98510bbaf1f912b11
Robert <robethuku11@gmail.com>
2022-11-28 07:42:00 +0300

    finish question2

---
+++ app/question1.js
@@ -0,0 +1,32 @@
+
+
+
+//function
+
+function getGrade(){
+  // get user input
+  let userMarks = parseInt(prompt('Input your marks'));
+  //initialize an empty variable to hold the result
+  let result;
+
+  // condition step1 check marks isnt more than 100
+
+  if(userMarks > 100) {
+    result = alert("Enter Valid Grade!");
+  } else if(userMarks >= 79) {                // Check if marks if more than 79
+    result = 'A';
+  } else if(userMarks >= 60) {                //checks if input is more or equal to 60
+    result = "B";
+  } else if(userMarks >= 59) {                // checks if input is more or equal to 59
+    result = "c";
+  } else if(userMarks >= 40) {                // checks if inpit is more or equal to 49
+    result = "D";
+  } else {                                    // checks for user input less than 40
+    result = "E"
+  }
+
+  return result;
+ 
+
+}
+
---
+++ app/question2.js
@@ -0,0 +1,22 @@
+// speed limit 
+let speedLimit = 70;
+
+//function
+function speedLimitToken(userInput) {
+  // get the value of excess speed
+  let diffInSpeed = userInput - speedLimit;
+  let result;
+
+  //check the demrit point condition when above speed limit
+  if(userInput > speedLimit && userInput % 5 === 0 && diffInSpeed/5 >= 12 ) {
+    result = "Revoked";
+  } else if( userInput > speedLimit && diffInSpeed % 5 === 0) {              // returns the merit points
+    result = `${diffInSpeed/5} merit points`;
+  } else {
+    result = "Ok";
+  }
+
+  return result
+
+} 
+
